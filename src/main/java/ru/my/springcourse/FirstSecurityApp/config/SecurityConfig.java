186f6d9d1b9548577f432f225823210263875cb8
6akov <egorka.shestakov.2017@mail.ru>
2024-08-27 07:59:30 +0300

    Encoding

--- src/main/java/ru/my/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/my/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -55,6 +56,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/my/springcourse/FirstSecurityApp/services/PeopleService.java
+++ src/main/java/ru/my/springcourse/FirstSecurityApp/services/PeopleService.java
@@ -1,6 +1,8 @@
 package ru.my.springcourse.FirstSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.my.springcourse.FirstSecurityApp.models.Person;
@@ -12,10 +14,12 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class PeopleService {
     private PeopleRepository peopleRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public PeopleService(PeopleRepository peopleRepository) {
+    public PeopleService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<Person> findByUsername(String username) {
@@ -23,6 +27,7 @@ public class PeopleService {
     }
     @Transactional
     public void save(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
